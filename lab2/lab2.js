7de8cb6b9d2e4274c44577b5238eb019f44535a0
vvp1061 <archie.geek@gmail.com>
2022-05-16 14:43:48 +0300

    Fibonachi added

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,21 +1,37 @@
 'use strict';
-function pow(x,n) {
-return x**n;
+
+function pow(x, n) {
+    return x ** n;
 }
 
-function sumTo(n){
-let sum=0;
-for (let i=1;i<=n;i++){
-sum = sum+i; }
-return sum;
+function sumTo(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+    return sum;
 }
 
-function factorial(n){
-let multiplier = 1;
-let i=1;
-while (i<=n) {
-    multiplier*=i 
-    i++;
+function factorial(n) {
+    let multiplier = 1;
+    let i = 1;
+    while (i <= n) {
+        multiplier *= i
+        i++;
+    }
+    return multiplier;
 }
-return multiplier;
+
+function fib(n) {
+    let a = 1n;
+    let b = 1n;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
