bd36e4de2ae307c775bb6f45a7c030266b029987
vvp1061 <archie.geek@gmail.com>
2022-05-16 14:17:36 +0300

    lab2

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,3 +1,21 @@
 'use strict';
-let x = 16;
-console.log(x);
+function pow(x,n) {
+return x**n;
+}
+
+function sumTo(n){
+let sum=0;
+for (let i=1;i<=n;i++){
+sum = sum+i; }
+return sum;
+}
+
+function factorial(n){
+let multiplier = 1;
+let i=1;
+while (i<=n) {
+    multiplier*=i 
+    i++;
+}
+return multiplier;
+}
