f1b766f56e23a907807ab6e52bd63bd9a5d0426a
vvp1061 <archie.geek@gmail.com>
2022-06-07 23:05:00 +0300

    3

--- JS/lab3/lab3.js
+++ JS/lab3/lab3.js
@@ -1,10 +1,7 @@
 'use strict';
 
 function getDecimal(num) {
-    let a = num.slice(num.indexOf('.'));
-    if (num.indexOf(".") == -1) {
-        return 0;
-    }
+    let a = num.slice(num.indexOf("."));
     let str = 0 + a;
     if (Number(num) < 0) {
         str = 1 - Number(str);
@@ -14,85 +11,38 @@ function getDecimal(num) {
 }
 
 function ucFirst(str) {
-    if (!str) return str;
-
-    return str[0].toUpperCase() + str.slice(1);
+    return (str[0].toUpperCase() + str.slice(1));
 }
 
 function checkSpam(str) {
-    let firstWord = 'viagra';
-    let secondWord = 'xxx';
-    let string = str.toLowerCase();
-    let result;
-
-    if (checkTheWord(firstWord) || checkTheWord(secondWord)) {
+    str = str.toLowerCase();
+    if ((str.includes('xxx')) || (str.includes('viagra')))
         return true;
-    }
-    return false;
-
-    function checkTheWord(word) {
-        for (let i = 0; i < string.length - 1; i++) {
-            if (string[i] == word[0]) {
-                result = string.slice(i, i + word.length);
-                if (result == word) {
-                    return true;
-                } else return false;
-            } else return false;
-        }
-    }
+    else return false;
 }
-
 
 function truncate(str, maxlength) {
     if (str.length > maxlength)
-        str = str.slice(0, maxlength - 1) + '\u{2026}';
+        str = str.slice(0, maxlength) + '\u{2026}';
     return str;
 }
-
 
 function camelize(str) {
-    return str
-        .split('-')
-        .map(
-            (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-        )
-        .join('');
-}
-
-function fibs(N) {
-    let a = [];
-    let str = '';
-    if (!Number.isNaN(N))
-        if (N == 0) str = 0;
-        else
-            for (let j = 0; j < N; ++j) {
-                a[j] = fib(j);
-                str += a[j] + ' ';
-            }
-    else str = NaN;
-
-    return str;
-
-    function fib(j) {
-        let a = 1 n;
-        let b = 1 n;
-        let c;
-        if (j == 0) return 0;
-        if (j == 1) return 1;
-        if (j == 2) return 1;
-        for (let i = 3; i <= j; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        if (b == 1 n)
-            b = 0;
-        return b;
+    let id;
+    while (str.includes("-")) {
+        id = str.indexOf('-');
+        str = str.slice(0, id) + str[id + 1].toUpperCase() + str.slice(id + 2);
     }
+    return str;
 }
 
+function arrReverseSorted(arr) {
+    let arr_reserve = [];
 
+    arr_reserve = arr.sort((a, b) => b - a);
 
+    alert(arr_reserve);
+}
 
 function sum() {
     let result = 0;
